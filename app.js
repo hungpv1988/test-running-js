f39368eabed1263d4c597cbddd95b1dd73ec39ea
hungpv1988@gmail.com <hung@EPVNLPTHUNG>
2023-04-27 10:27:57 +0700

    first full embeded script from CDN

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require("path");
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
-// app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/public'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname,'views')));
 app.listen(5001, () => {
